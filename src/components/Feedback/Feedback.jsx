2f01c772f7e79ee6fe0fa0fb39676b4dae22a1c0
Vladislav <cheremisin0610@mail.ru>
2022-06-04 20:16:18 +0300

    add slider

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Slider from 'react-slick';
 import styles from './Feedback.module.scss';
 import FeedbackCard from './FeedbackCard/FeedbackCard';
 
-const Feedback = (props) => {
-  return (
-    <div className={styles.feedback}>
-      <h2 className={styles.title}>отзывы НАШИХ КЛИЕНТОВ</h2>
-      <FeedbackCard />
-    </div>
-  );
-};
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+    };
+    return (
+      <div>
+        <h2 className={styles.title}>отзывы НАШИХ КЛИЕНТОВ</h2>
+        <Slider {...settings}>
+          <div>
+            <FeedbackCard />
+          </div>
+          <div>
+            <FeedbackCard />
+          </div>
+          <div>
+            <FeedbackCard />
+          </div>
+          <div>
+            <FeedbackCard />
+          </div>
+          <div>
+            <FeedbackCard />
+          </div>
+          <div>
+            <FeedbackCard />
+          </div>
+        </Slider>
+      </div>
+    );
+  }
+}
 
-export default Feedback;
+// const Feedback = (props) => {
+//   return (
+//     <div className={styles.feedback}>
+//       <h2 className={styles.title}>отзывы НАШИХ КЛИЕНТОВ</h2>
+//       <FeedbackCard />
+//     </div>
+//   );
+// };
+
+// export default Feedback;
